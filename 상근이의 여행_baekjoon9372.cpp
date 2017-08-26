e26b013b411e806ce15413c18a4553af2f3e3b79
hyeseonko <hsko0926@gmail.com>
2017-08-26 19:31:09 +0900

    Add files via upload

---
+++ 상근이의 여행_baekjoon9372.cpp
@@ -0,0 +1,26 @@
+/*
+	TITLE: FLYING SAFELY
+	BAEKJOON 9372
+	CATEGORY: BFS, MST
+	DATE: 2017-08-26
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int T; scanf("%d", &T);
+	while (T--)
+	{
+		int N, M; scanf("%d %d", &N, &M);
+		while (M--)
+		{
+			int from, to; scanf("%d %d", &from, &to);
+		}
+		printf("%d\n", N - 1);
+	}
+	return 0;
+}
+
+// ³¬¿´´Ù..
